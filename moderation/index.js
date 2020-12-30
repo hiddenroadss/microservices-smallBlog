65f34b36d43832ee67037ae1de25ef3047802650
taras <hiddenroadss@gmail.com>
2020-12-30 17:48:29 +0200

    initial

---
+++ moderation/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body
+    if (type === 'CommentCreated') {
+        const newStatus = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status: newStatus
+            }
+            
+        })
+    }
+    res.send({})
+})
+
+
+app.listen(4003, () => console.log('Listening on port 4003'))
